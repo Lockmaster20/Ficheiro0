60d16c9dc7b0f970ccb89c4c1d630651b099d832
Diogo Fernandes <diogoafernandes20@gmail.com>
2020-02-13 11:32:40 +0000

    Adicionar arquivos de projeto.

---
+++ Ficheiro0/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Ficheiro0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private void Button1_Click(object sender, EventArgs e)
+        {
+             label1.Text = File.ReadAllText(Path.Combine(docPath, "Texto.txt"));
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string lines = textBox1.Text;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Texto.txt")))
+            {
+                outputFile.WriteLine(lines);
+            }
+        }
+    }
+}
